8da740f8303a16a5bee79c3d54b13e44b8c35eab
Naruto <singhvedprakash075@gmail.com>
2020-05-05 16:54:13 +0530

    Code Refactor

--- Miscellaneous/navbar/app.js
+++ Miscellaneous/navbar/app.js
@@ -2,6 +2,5 @@ var burger = document.querySelector(".burger");
 var nav1 = document.querySelector("ul");
 
 burger.addEventListener("click", function(){
-    console.log("Clicked")
     nav1.classList.toggle("view");
 })
