31b98e6024680f378021ed54f72aa1813bd2dde0
Naruto <singhvedprakash075@gmail.com>
2020-05-05 17:28:50 +0530

    Minor Improvements

--- Miscellaneous/navbar/app.js
+++ Miscellaneous/navbar/app.js
@@ -1,6 +1,6 @@
 var burger = document.querySelector(".burger");
-var nav1 = document.querySelector("ul");
+var nav = document.querySelector("ul");
 
 burger.addEventListener("click", function(){
-    nav1.classList.toggle("view");
+    nav.classList.toggle("view");
 })
